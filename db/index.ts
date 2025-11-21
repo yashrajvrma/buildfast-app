00dd27b7a405396949b42421be7cf3b21866e370
yashrajvrma <vermayashraj2005@gmail.com>
2025-11-22 02:04:36 +0530

    build: migrate to prisma v7

--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
