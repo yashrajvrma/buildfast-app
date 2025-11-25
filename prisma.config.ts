e6a772bfb8361f114843e11bd9436b61fecf73fe
yashrajvrma <vermayashraj2005@gmail.com>
2025-11-25 17:36:31 +0530

    build: fix .env error in prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,11 +3,11 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
   migrations: {
     path: "prisma/migrations",
     seed: "tsx prisma/seed.ts",
-  },
-  datasource: {
-    url: env("DATABASE_URL"),
   },
 });
